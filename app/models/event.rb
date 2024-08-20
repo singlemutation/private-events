e09c7e31557b87e0acdf64862bebb3d0b157e09d
singlemutation <164826575+singlemutation@users.noreply.github.com>
2024-08-20 12:27:47 -0400

    Add simple navbar to app html

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attended_events, foreign_key: :event_attended_id
   has_many :attendees, through: :attended_events, source: :event_attendee
+
+  scope :past, -> { where("date < ?", Time.now) }
+  scope :future, -> {where("date > ?", Time.now) }
+
 end
